1eecfdbb898b4f55596ab17eb46a35cba445ba4c
Mengyanz <u6015325@anu.edu.au>
2019-04-15 21:17:40 +1000

    update experiment

--- QuantUCB/UCB_discrete.py
+++ QuantUCB/UCB_discrete.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import numpy as np
 from collections import defaultdict
+from qreg import QRegressor
 
 class UCB_discrete(ABC):
     """Base class for UCB algorithms of finite number of arms.
@@ -10,7 +11,7 @@ class UCB_discrete(ABC):
         Arguments
         ------------------------------------
         env: list
-            sequence of distribution reward of arms
+            sequence of instances of Environment (distribution reward of arms)
         num_rounds: int
             total number of rounds
         bestarm: int
@@ -55,7 +56,8 @@ class UCB_discrete(ABC):
         the index of arm with the maximum ucb
         """
         
-    def regret(self, idx, reward):
+    #def regret(self, idx, reward):
+    def regret(self):
         """Calculate cumulative regret and record it
 
         Parameters
@@ -69,13 +71,21 @@ class UCB_discrete(ABC):
         -----------------------------
         None
         """
+
+        """
         if idx != self.bestarm:
             self.bestActionCumulativeReward += self.env[self.bestarm].sample()
         else:
             self.bestActionCumulativeReward += reward
         CumulatveRegret = self.bestActionCumulativeReward - self.cumulativeReward
         self.cumulativeRegrets.append(CumulatveRegret)
-        
+        """
+        my_regret = 0
+        for key, value in self.sample_rewards.items():
+            mu_diff = self.env[self.bestarm].loc - self.env[key].loc
+            my_regret += mu_diff * len(value)
+        self.cumulativeRegrets.append(my_regret)
+    
     def init_reward(self):
         """pull each arm once and get the rewards as the inital reward 
         """
@@ -86,14 +96,16 @@ class UCB_discrete(ABC):
     def play(self):
         """Simulate UCB algorithms for specified rounds.
         """
+        
         self.init_reward()
         for i in range(len(self.env),self.num_rounds):
-            print('Round: ', i)
+            #print('Round: ', i)
             idx = self.argmax_ucb(i)
-            print('select arm: ', idx)
+            #print('select arm: ', idx)
             reward = self.sample(idx)
             #self.num_played[idx] += 1
-            self.regret(idx, reward)
+            #self.regret(idx, reward)
+            self.regret()
         return self.cumulativeRegrets
 
     def sample(self, idx):
@@ -121,7 +133,7 @@ class QuantUCB(UCB_discrete):
         super().__init__(env, num_rounds, bestarm)
 
     def quantile(self, data, alpha):
-        """calculate alpha-quantile for given data samples.
+        """calculate empirical alpha-quantile for given data samples.
 
         Parameters
         -----------------------------------
@@ -137,7 +149,7 @@ class QuantUCB(UCB_discrete):
         """
         
         data = np.sort(data)
-        idx = int(len(data) * alpha) - 1
+        idx = int(len(data) * alpha)
         return data[idx]
 
     def sigmoid(self, x):
@@ -161,7 +173,7 @@ class QuantUCB(UCB_discrete):
         """
         #alpha = np.sqrt(np.log(t)/n_selected)
         alpha = self.sigmoid(np.log(np.sqrt(10.0/n_selected)))
-        print('alpha: ', alpha)
+        #print('alpha: ', alpha)
         return alpha
 
         # ugly control here, need to be fixed
@@ -169,6 +181,126 @@ class QuantUCB(UCB_discrete):
         #    return 0.8
         #else: 
         #    return alpha
+
+class QuantUCB_Gau(UCB_discrete):
+    """Quantile UCB with Gaussian rewards. 
+       For now, we assume the env is exposed to calculate alpha but not exposed to player.
+       For now, instead of using estimated quantile, we first plug alpha into true quantile function.
+    """
+    def __init__(self, env, num_rounds, bestarm):
+        super().__init__(env, num_rounds, bestarm)
+
+    def emp_quantile(self, data, alpha):
+        """calculate empirical alpha-quantile for given data samples.
+
+        Parameters
+        -----------------------------------
+        data: list
+            sequence of sample rewards
+        alpha: 
+            level of quantile
+
+        Return
+        ------------------------------------
+        quantile: float
+            alpha level quantile of given data
+        """
+        
+        data = np.sort(data)
+        idx = int(len(data) * alpha)
+        return data[idx]
+
+    def linear_inter_quant(self, alpha, data):
+        """implement linear interpolation for quantile estimation.
+        
+        Parameters
+        -----------------------------------
+        data: list
+            sequence of sample rewards
+        alpha: 
+            level of quantile
+
+        Return
+        ------------------------------------
+        quantile: float
+            alpha level quantile of given data
+        """
+        size = len(data) 
+        data = list(data)
+        data.append(-2)
+        data = np.sort(data)
+        s = int(alpha * size) 
+        rate = (data[s + 1] - data[s]) * size
+        #b = data[s] * (1-s) - data[s+1]
+        #return rate * alpha + b
+        return rate * (alpha - float(s)/size) + data[s]
+
+    def quantile(self, i, alpha):
+        """Calculate true quantile for distribution of given alpha
+        """
+        #return self.env[i].loc + self.env[i].scale * np.sqrt(2) * self.inv_erf(2 * alpha - 1)
+        return np.mean(self.sample_rewards[i]) + self.env[i].scale * np.sqrt(2) * self.inv_erf(2 * alpha - 1)
+    
+    def erf(self,x):
+        """Approximate erf with maximum error 1.5 * 10^(-7)
+        """
+        a1=  0.0705230784
+        a2=  0.0422820123
+        a3=  0.0092705272
+        a4=  0.0001520143
+        a5=  0.0002765672
+        a6=  0.0000430638
+
+        return 1.0 - 1.0/(1+ a1 * x + a2 * x**2 + a3 * x**3 + a4 * x**4 + a5 * x**5 + a6 * x**6)**16
+
+    def inv_erf(self,x):
+        "Approximate inverse erf"
+        a = 0.140012
+        temp = 2.0/(np.pi * a) + np.log(1-x**2)/2.0
+        return np.sign(x) * np.sqrt((np.sqrt(temp**2 - np.log(1-x**2)/a) - temp))
+
+    def alpha_level(self, t, n_selected):
+        """calculate the alpha level for quantile.
+        
+        Parameters
+        -----------------------------------
+        t: int
+            the current round
+
+        n_selected: int
+            the number of times arm j was selected up to the current round
+
+        Return
+        -----------------------------------
+        alpha: float 
+            between 0 and 1, alpha level for quantile
+        """
+        return 0.5 * (self.erf(2 * np.sqrt(np.log(t)/n_selected)) + 1)
+
+    def argmax_ucb(self, t):
+        """Compute upper confidence bound 
+
+        Parameters
+        --------------------------------
+        t: int
+            the number of current round
+
+        Return
+        --------------------------------
+        the index of arm with the maximum ucb
+        """
+        ucbs = []
+        for arm in sorted(self.sample_rewards.keys()):
+            reward = self.sample_rewards[arm]
+            quant = self.quantile(arm, self.alpha_level(t, len(reward)))
+            mean_reward = np.mean(reward)
+            #quant = self.linear_inter_quant(self.alpha_level(t, len(reward)), reward)
+            #print('arm ', arm, 'mean reward: ', mean_reward, ' quant:', quant)
+            #print('mean + quant: ', mean_reward + quant)
+            ucbs.append(mean_reward + quant)
+            
+        assert len(ucbs) == len(self.env)
+        return np.argmax(ucbs)
 
 class QuantUCB_MUQ(QuantUCB):
     """QuantUCB by maximizing upper quantiles
@@ -192,8 +324,8 @@ class QuantUCB_MUQ(QuantUCB):
         for arm in sorted(self.sample_rewards.keys()):
             reward = self.sample_rewards[arm]
             quant = self.quantile(reward, self.alpha_level(t, len(reward)))
-            print('arm ', arm, 'mean reward: ', np.mean(reward), ' quant:', quant)
-            print('np.sqrt(np.log(t)) * quant: ', np.sqrt(np.log(t)/len(reward)) * quant)
+            #print('arm ', arm, 'mean reward: ', np.mean(reward), ' quant:', quant)
+            #print('np.sqrt(np.log(t)) * quant: ', np.sqrt(np.log(t)/len(reward)) * quant)
             ucbs.append(np.mean(reward) + np.sqrt(np.log(t)/len(reward)) * quant)
             
         assert len(ucbs) == len(self.env)
@@ -222,7 +354,7 @@ class QuantUCB_MMQD(QuantUCB):
             reward = self.sample_rewards[arm]
             quant = self.quantile(reward, self.alpha_level(t, len(reward)))
             median = self.quantile(reward, 0.5)
-            print('arm ', arm, 'mean reward: ', np.mean(reward), ' quant:', quant, ' median: ', median)
+            #print('arm ', arm, 'mean reward: ', np.mean(reward), ' quant:', quant, ' median: ', median)
             ucbs.append(np.mean(reward) + quant - median)
             
         assert len(ucbs) == len(self.env)
@@ -272,7 +404,7 @@ class CVaRUCB(UCB_discrete):
         """
         alpha = np.sqrt(np.log(t)/n_selected)
         return self.sigmoid(alpha)
-        #print(alpha)
+        ##print(alpha)
 
         # ugly control here, need to be fixed
         #if alpha >= 1:
@@ -333,10 +465,19 @@ class Environment():
         self.scale = scale
         self.skewness = skewness
 
+    
     def sample(self):
+        """uniformly generate x (between 0 and 1), 
+           generate normal or skewed normal samples
+        
         sigma = self.skewness/np.sqrt(1.0 + self.skewness**2) 
         u0,v = np.random.randn(2)
         u1 = sigma*u0 + np.sqrt(1.0-sigma**2) * v 
         if u0 >= 0:
             return u1*self.scale + self.loc
         return (-u1)*self.scale + self.loc 
+        """
+
+    def sample(self):
+        return np.random.normal(self.loc, self.scale)
+
