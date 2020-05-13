53ac95e402457e1a05ffbc6bf2c5cbdf09cd2caa
Yiping  Su <yiping.su@anu.edu.au>
2020-05-13 15:35:35 +1000

    moving p2 to a python file

--- QuantEst/sgd_est_experiment/Quantile_procs.py
+++ QuantEst/sgd_est_experiment/Quantile_procs.py
@@ -1,15 +1,20 @@
 import numpy as np
 import math
 from Method_sgd_frugal_adaptive import get_adaptive_procs, get_frugal_procs, get_sgd_procs
+from Method_p2 import get_ext_p2_procs
 
 def get_procs(dataset, tau_lst, method_name, **kwargs):
     if len(dataset.shape)!= 1: 
         raise Exception('Dataset for get_procs() of wrong shape:' + str(dataset.shape)+ ', should be 1d array')
     print ("get_procs() using method {}".format(method_name))
 
+    print (method_name)
     method_dict = {
         'sgd': get_sgd_procs(dataset, tau_lst, **kwargs),
         'frugal': get_frugal_procs(dataset, tau_lst),
-        'adaptive': get_adaptive_procs(dataset, tau_lst, **kwargs)
+        'adaptive': get_adaptive_procs(dataset, tau_lst, **kwargs),
+        'p2': get_ext_p2_procs(dataset, tau_lst),
     }
     return method_dict.get(method_name)
+
+# get_procs(np.zeros((1000)), [0.1, 0.2], 'p2' , stepsize='anyway')
