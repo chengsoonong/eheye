2f9a373c4e6645ac24880e6741f754ab19020b94
Yiping  Su <yiping.su@anu.edu.au>
2020-06-15 08:27:24 +1000

    SAg implementation, two images do not match

--- QuantEst/sgd_est_experiment/Quantile_procs.py
+++ QuantEst/sgd_est_experiment/Quantile_procs.py
@@ -3,11 +3,12 @@ import math
 from Method_sgd_frugal_adaptive import get_adaptive_procs, get_frugal_procs, get_sgd_procs
 from Method_p2 import get_p2_procs
 from Method_shiftQ import get_shiftQ_procs
+from Method_sag import get_SAG_procs
 
-def get_procs(dataset, tau_lst, method_name, **kwargs):
+def get_procs(dataset, tau_lst, method_name, *args):
     if len(dataset.shape)!= 1: 
         raise Exception('Dataset for get_procs() of wrong shape:' + str(dataset.shape)+ ', should be 1d array')
-
+    print (tau_lst, method_name)
     # print (method_name)
     method_dict = {
         'SGD' : get_sgd_procs,
@@ -15,6 +16,7 @@ def get_procs(dataset, tau_lst, method_name, **kwargs):
         'Adaptive': get_adaptive_procs,
         'P2': get_p2_procs,
         'shiftQ': get_shiftQ_procs,
+        'SAG': get_SAG_procs,
     }
     # method_dict = {
     #     'SGD': get_sgd_procs(dataset, tau_lst, **kwargs),
@@ -24,7 +26,7 @@ def get_procs(dataset, tau_lst, method_name, **kwargs):
     # }
 
     # if method_name in method_dict.keys():
-    return  method_dict.get(method_name)(dataset, tau_lst, **kwargs)
+    return  method_dict.get(method_name)(dataset, tau_lst, *args)
     # else:
     #     raise Exception ("Method name {} is wrong! Please check the avaliable ones {}"
     #                     .format(method_name,
